5c6c5c5e0d9d6f64211b55438a9092d31b6d2b24
Sandisz Thieme <sandiszthieme@gmail.com>
2026-06-11 14:04:34 -0600

    fix screen size

--- src/components/FullScreenSection.jsx
+++ src/components/FullScreenSection.jsx
@@ -2,8 +2,8 @@ export default function Section({ id, children }) {
   return (
     <section id={id} className="w-full flex justify-center py-1 scroll-mt-24">
       <div
-        className="custom_page bg-white dark:bg-neutral-800 lg:w-[98%] w-[92%] h-[99vh] max-w-full mx-0 sm:mx-2 lg:ml-0 ml-5 px-1 sm:px-5
-                   py-10 sm:py-2 transition-colors duration-500 border border-lime-400 dark:border-white rounded-2xl"
+        className="custom_page bg-white dark:bg-neutral-800 lg:w-[98%] w-[92%] h-[99vh] max-w-full mx-0 sm:mx-2 lg:ml-0 ml-1 px-0
+                   py-6 sm:py-2 transition-colors duration-500 border border-lime-400 dark:border-white rounded-2xl"
       >
         {children}
       </div>
